79555053080ae087c655fd8ff4126b531ef03f48
Vaseliy <bonanava@yandex.ru>
2023-02-17 11:37:51 +0300

    format

--- src/components/new-task-form/new-task-form.js
+++ src/components/new-task-form/new-task-form.js
@@ -1,45 +1,42 @@
-import { Component } from "react";
-import "./new-task-form.css"
+import { Component } from 'react';
+import './new-task-form.css';
 
 export default class NewTaskForm extends Component {
+  state = {
+    label: '',
+  };
 
-    state = {
-        label: ""
-    }
+  onLabelChange = (event) => {
+    this.setState({
+      label: event.target.value,
+    });
+  };
 
-    onLabelChange = (event) => {
-        this.setState(({ 
-            label: event.target.value
-        }));
-    };
+  onSubmit = (event) => {
+    event.preventDefault();
 
-    onSubmit = (event) => {
-        event.preventDefault();
+    if (this.state.label.trim()) this.props.onItemAdded(this.state.label);
+    else alert("O_0ps! You didn't finish the task");
 
-        if (this.state.label.trim())
-            this.props.onItemAdded(this.state.label);
-        else
-            alert("O_0ps! You didn't finish the task");
+    this.setState({
+      label: '',
+    });
+  };
 
-        this.setState(({ 
-            label: ""
-        }));
-    };
-
-    render() {
-        return (
-        <header className="app-header">
-            <h1 className="app-title">todos</h1>
-            <form className="new-task-form"
-                  onSubmit={this.onSubmit}>
-                <input className="new-todo"
-                       placeholder="What needs to be done?"
-                       onChange={this.onLabelChange}
-                       value={this.state.label}
-                       autoFocus>
-                </input>
-            </form>
-        </header>
-        );
-    };
-};
+  render() {
+    return (
+      <header className="app-header">
+        <h1 className="app-title">todos</h1>
+        <form className="new-task-form" onSubmit={this.onSubmit}>
+          <input
+            className="new-todo"
+            placeholder="What needs to be done?"
+            onChange={this.onLabelChange}
+            value={this.state.label}
+            autoFocus
+          ></input>
+        </form>
+      </header>
+    );
+  }
+}
